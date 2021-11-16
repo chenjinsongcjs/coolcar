64ea19a478a3a3688edbc79a6d700e2cf63e2b5c
cjs <13887096914@163.com>
2021-11-16 22:21:03 +0800

    函数闭包实验

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -24,6 +24,7 @@ Page({
         canIUseGetUserProfile: true
       })
     }
+    // this.updateMotto()  函数闭包测试，减少全局状态变量的定义
   },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
@@ -45,5 +46,23 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  updateMotto(){
+    let count = 0
+    let shouldStop = false
+    setTimeout(()=>{
+      shouldStop = true
+    },10000)
+    let update = ()=>{
+      count++
+      if(!shouldStop){
+        this.setData({
+          motto: `update motto ${count}`
+        },()=>{
+          update()
+        })
+      }
+    }
+    update()
   }
 })
